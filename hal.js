28fe30654750084a8e286c3760bc4c4160fa3f0f
ahmet <off.ahmethuseyindok@gmail.com>
2019-12-12 22:31:17 +0300

    template is ok

--- hal.js
+++ hal.js
@@ -3,6 +3,10 @@ class Atik{
         return new RenderNode(new Element(tag,attributes,children));    
     }
 
+    static createElementFromTemplate(temp ,props){
+        return createElementFromTemplate(temp,props);
+    }
+
     static render(renderNode , elem){
         elem.innerHTML = ""
         elem.appendChild(renderNode.render());
@@ -68,8 +72,8 @@ class RenderNode{
 
         //clear child
         this.htmlNode.innerHTML = "";
-        
         for (const child of this.elem.children) {
+              
             //checks if a render object or just a string
             if(typeof child == "string"){
                 this.htmlNode.appendChild(document.createTextNode(child));    
@@ -84,6 +88,103 @@ class RenderNode{
 
     
 
+}
+
+function createElementFromTemplate(temp , props){
+    let node = document.createElement("div");
+    node.innerHTML =  temp;
+    return nodeToAtikElement(node.children[0],props); 
+}
+
+function nodeToAtikElement(node,props){
+    
+    let nodeName = node.localName;
+    
+    let attrObj = {};
+    const evalReg = /{{[^}]*}}/g;
+    
+    if(node.nodeName == "#text"){
+        return node.wholeText.replace(evalReg, (match) => {
+            let exp = match.slice(2,-2);
+            return eval(exp);
+        });
+    }
+
+    let tag = nodeName.startsWith('a-') ? eval(nodeName.slice(2)) : nodeName ;
+    
+    let children = [];
+
+    let aProps = [];
+
+    for (const {name,value} of node.attributes) {
+
+        let res = value.replace(evalReg, (match) => {
+            let exp = match.slice(2,-2);
+            return eval(exp);
+        });
+
+        if(name.startsWith('a-')){
+            aProps.push({name,value})
+        }else{
+            attrObj[name] = res;    
+        }
+
+        
+    } 
+
+    for( const {name ,value} of aProps){
+        
+        switch(name){
+            case 'a-show': 
+                if(!eval(value))  return "";
+                break;
+            case 'a-list': 
+                let [keyEv,listEv] = value.split("in");
+                let list = eval(listEv);
+                let key = keyEv.trim();
+                let children = [];
+                
+                for (const item of list) {
+                    
+                    let itemChildren = [];
+                    
+                    for (const child of node.childNodes) {
+                        let atikParam = nodeToAtikElement(child,{...props,[key]:item});
+                        if(atikParam){
+                            if(atikParam.length){
+                                itemChildren.push(...atikParam)
+                            }else{
+                                itemChildren.push(atikParam)
+                            }   
+                        }
+                    }
+                    
+                    children.push(Atik.createElement(tag,attrObj,...itemChildren))
+                }
+                    
+
+                return children;
+                break;
+        }
+    }
+    
+    
+    
+    for (const child of node.childNodes) {
+            let atikParam = nodeToAtikElement(child,props);
+            
+            if(atikParam){
+                if(atikParam.length){
+                    children.push(...atikParam)
+                }else{
+                    children.push(atikParam)
+                }   
+            }
+    }
+
+
+    
+    return Atik.createElement(tag,attrObj,...children);
 }
 
 class Hal{
